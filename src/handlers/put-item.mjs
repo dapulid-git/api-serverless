7abee610d9ce6e02f704aa6e7005efcdfdf4c2e1
dapulid-git <dpulido022@gmail.com>
2024-06-23 14:53:45 -0500

    Creacion del proyecto incial por medio de SAM y creacion del pipeline de CI/CD

---
+++ src/handlers/put-item.mjs
@@ -0,0 +1,38 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
+
+const client = new DynamoDBClient({});
+const ddbDocClient = DynamoDBDocumentClient.from(client);
+
+const tableName = process.env.SAMPLE_TABLE;
+
+export const putItemHandler = async (event) => {
+    if (event.httpMethod !== 'POST') {
+        throw new Error(`postMethod only accepts POST method, you tried: ${event.httpMethod} method.`);
+    }
+
+    const body = JSON.parse(event.body);
+
+    const id = body.id;
+    const name = body.name;
+
+    var params = {
+        TableName: tableName,
+        Item: { id: id, name: name }
+    };
+
+    try {
+        const data = await ddbDocClient.send(new PutCommand(params));
+    } catch (err) {
+        throw new Error(err.stack);
+    }
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(body)
+    };
+
+    // All log statements are written to CloudWatch
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+    return response;
+};
