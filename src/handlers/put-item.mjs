b8cd3639d5417afdeed03ecd9fa197e7423381c2
dapulid-git <dpulido022@gmail.com>
2024-06-23 16:58:19 -0500

    Se agrega la validacion del id del la tabla usuarios

--- src/handlers/put-item.mjs
+++ src/handlers/put-item.mjs
@@ -1,5 +1,6 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBDocumentClient, GetCommand } from '@aws-sdk/lib-dynamodb';
 
 const client = new DynamoDBClient({});
 const ddbDocClient = DynamoDBDocumentClient.from(client);
